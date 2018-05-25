20002a2eefc478e13dee3d643a9dd74ec6600566
Pavel <pashcal09@gmail.com>
2018-05-25 13:23:25 +0300

    Droid: remove bootstraps from modules

--- src/app-ropio/AppRopio.ECommerce/Basket/Droid/Bootstrap/VisibilityPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace AppRopio.ECommerce.Basket.Droid.Bootstrap
-{
-    public class VisibilityPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
-    {
-    }
-}
