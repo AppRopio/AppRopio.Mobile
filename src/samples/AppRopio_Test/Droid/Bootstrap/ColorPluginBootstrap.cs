a11d893398900b3b32f3a67e6ef910f1322b0678
quak <babaeva.irina@notissimus.com>
2018-09-17 16:08:38 +0300

    Droid: filters, add text color

---
+++ src/samples/AppRopio_Test/Droid/Bootstrap/ColorPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace AppRopio.Test.Droid.Bootstrap
+{
+    public class ColorPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Color.PluginLoader>
+    {
+    }
+}
