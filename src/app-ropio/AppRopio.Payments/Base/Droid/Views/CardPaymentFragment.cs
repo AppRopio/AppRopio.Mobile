698f8041b364858234560576d80a03a190470c59
pocheshire <maksevtukh@gmail.com>
2018-11-19 15:56:16 +0500

    cloudpayment droid implementation

--- src/app-ropio/AppRopio.Payments/Base/Droid/Views/CardPaymentFragment.cs
+++ src/app-ropio/AppRopio.Payments/Base/Droid/Views/CardPaymentFragment.cs
@@ -1,14 +1,31 @@
-﻿using AppRopio.Base.Droid.Views;
+﻿using Android.OS;
+using Android.Views;
+using Android.Webkit;
+using AppRopio.Base.Droid.Views;
+using AppRopio.Payments.Core.Services;
 using AppRopio.Payments.Core.ViewModels;
+using MvvmCross.Platform;
 
 namespace AppRopio.Payments.Droid.Views
 {
     public class CardPaymentFragment : CommonFragment<ICardPaymentViewModel>
     {
+        protected WebView WebView { get; private set; }
+
         public CardPaymentFragment()
             : base (Resource.Layout.app_payment_card)
         {
             Title = "Оплата картой";
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            WebView = view.FindViewById<WebView>(Resource.Id.webView);
+
+            var threeDSService = Mvx.Resolve<IPayment3DSService>();
+            threeDSService.SetWebView(WebView);
         }
     }
 }
