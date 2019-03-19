f91637c35a40bf76f7ea7baa5858da03d1f4a72c
Serhii <Koshelnyk>
2019-03-19 14:23:26 +0200

    Added option to change currency symbol regardless of language

---
+++ src/app-ropio/AppRopio.Base/Core/Models/App/Locale.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace AppRopio.Base.Core.Models.App
+{
+    public class Locale
+    {
+        public string Name { get; set; }
+
+        public string CurrencySymbol { get; set; }
+    }
+}
