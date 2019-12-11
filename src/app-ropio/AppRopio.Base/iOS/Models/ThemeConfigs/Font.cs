9b993785dcf4f936384902a79f75f2c407cbf529
entdark <entdark@mail.ru>
2019-12-11 08:02:02 +0300

    iOS: use system fonts if the fonts palette is not presented

--- src/app-ropio/AppRopio.Base/iOS/Models/ThemeConfigs/Font.cs
+++ src/app-ropio/AppRopio.Base/iOS/Models/ThemeConfigs/Font.cs
@@ -22,6 +22,9 @@ namespace AppRopio.Base.iOS.Models.ThemeConfigs
             if (v == null)
                 return null;
 
+            string name = string.Empty;
+            string weight = string.Empty;
+
             if (!string.IsNullOrEmpty(v.Reference) && v.Reference.StartsWith("@", StringComparison.InvariantCulture))
             {
                 var slittedString = v.Reference.Replace("@", "").Split('/');
@@ -35,12 +38,40 @@ namespace AppRopio.Base.iOS.Models.ThemeConfigs
                                         .GetType()
                                         .GetProperties()
                                         .FirstOrDefault(x => x.GetCustomAttribute<JsonPropertyAttribute>().PropertyName == valuePart);
-
-                    return UIFont.FromName((string)property.GetValue(Theme.FontsPalette), v.Size);
+                    name = property.GetValue(Theme.FontsPalette) as string;
                 }
+                weight = valuePart;
+            }
+            else
+            {
+                name = v.Name;
             }
 
-            return UIFont.FromName(v.Name, v.Size);
+            if (string.IsNullOrEmpty(name))
+            {
+                UIFont font;
+                if (!string.IsNullOrEmpty(weight))
+                {
+                    var fontWeight = StringToWeight(weight);
+                    font = UIFont.SystemFontOfSize(v.Size, fontWeight);
+
+                    if (weight.ToLower() == "italic")
+                    {
+                        var fontDescriptor = font.FontDescriptor.CreateWithTraits(UIFontDescriptorSymbolicTraits.Italic);
+                        font = UIFont.FromDescriptor(fontDescriptor, 0.0f); //0.0f - keep the same size
+                    }
+                }
+                else
+                {
+                    font = UIFont.SystemFontOfSize(v.Size, UIFontWeight.Regular);
+                }
+                return font;
+            }
+            else
+            {
+                return UIFont.FromName(name, v.Size);
+            }
+
         }
 
         public object Clone()
@@ -51,6 +82,25 @@ namespace AppRopio.Base.iOS.Models.ThemeConfigs
                 Size = this.Size,
                 Reference = this.Reference
             };
+        }
+
+        private static UIFontWeight StringToWeight(string weight)
+        {
+            weight = weight.ToLower();
+            switch (weight)
+            {
+                default:
+                case "regular":
+                    return UIFontWeight.Regular;
+                case "light":
+                    return UIFontWeight.Light;
+                case "medium":
+                    return UIFontWeight.Medium;
+                case "semibold":
+                    return UIFontWeight.Semibold;
+                case "bold":
+                    return UIFontWeight.Bold;
+            }
         }
     }
 }
