2932240c52f3ffa2696bb32a6c257badfbb0fe9b
entdark <entdark@mail.ru>
2021-04-01 19:04:21 +0300

    Remade plugin loading in Payments.Best2Pay.Droid

--- src/app-ropio/AppRopio.Payments/Best2Pay/Droid/Plugin.cs
+++ src/app-ropio/AppRopio.Payments/Best2Pay/Droid/Plugin.cs
@@ -12,8 +12,8 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-using System;
 using AppRopio.Base.Core.Services.ViewLookup;
+using AppRopio.Payments.Best2Pay.Core;
 using AppRopio.Payments.Best2Pay.Core.ViewModels.Best2Pay;
 using AppRopio.Payments.Best2Pay.Droid.Views;
 using MvvmCross;
@@ -21,11 +21,15 @@ using MvvmCross.Plugin;
 
 namespace AppRopio.Payments.Best2Pay.Droid
 {
-    public class Plugin : IMvxPlugin
+    [MvxPlugin]
+    [Preserve(AllMembers = true)]
+    public class Plugin : BasePlugin
     {
-        public void Load()
+        public override void Load()
         {
-            var viewLookupService = Mvx.Resolve<IViewLookupService>();
+            base.Load();
+
+            var viewLookupService = Mvx.IoCProvider.Resolve<IViewLookupService>();
 
             viewLookupService.Register<IBest2PayViewModel, Best2PayFragment>();
         }
