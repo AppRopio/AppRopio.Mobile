dd63bd8f42d846063ac3daf310bf2dec23ddb81e
entdark <entdark@mail.ru>
2021-04-05 20:16:36 +0300

    Fixed compilation errors in AppRopio.Test.Droid

--- src/samples/AppRopio_Test/Droid/Setup.cs
+++ src/samples/AppRopio_Test/Droid/Setup.cs
@@ -1,14 +1,13 @@
-﻿using Android.Content;
-using AppRopio.Base.Droid.FCM;
+﻿using AppRopio.Base.Droid.FCM;
 using AppRopio.Navigation.Menu.Droid;
 using AppRopio.Navigation.Menu.Droid.Views;
 
 namespace AppRopio.Test.Droid
 {
-    public class Setup : MenuSetup
+	public class Setup : MenuSetup
     {
-        public Setup(Context applicationContext) 
-            : base(applicationContext)
+        public Setup() 
+            : base()
         {
             FcmSettings.Instance.ColorHex = "#FFD535";
             FcmSettings.Instance.IconResourceId = Droid.Resource.Drawable.logo;
